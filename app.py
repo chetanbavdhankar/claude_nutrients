6c9e853307d958e24b1359e66832c2449c52d10e
chetanbavdhankar <chetanbavdhankar@gmail.com>
2026-03-22 13:15:16 +0100

    Render compatibility

--- app.py
+++ app.py
@@ -221,4 +221,6 @@ def api_pantry_search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_ENV") != "production"
+    app.run(host="0.0.0.0", port=port, debug=debug)
